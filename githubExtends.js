ffe397a6ed3a50f66863fca867035cd55ae45e2b
Saqib Ameen <mrsaqibameen@gmail.com>
2020-06-29 13:22:33 +0500

    📦 NEW: testing for change

--- githubExtends.js
+++ githubExtends.js
@@ -1,1 +1,15 @@
-console.log("GitHub extends is running");
+// Get all the input fields on the settings page.
+let allInputs = document.querySelectorAll('p > input');
+
+let archiveInput = allInputs[3];
+
+console.log("archiveInput", archiveInput);
+
+// Select the input.
+archiveInput.value = 'saqibameen/learn-next-js';
+archiveInput.focus();
+
+document.execCommand('insertText', false, 'saqibameen/learn-next-js');
+
+// Check the value
+console.log("archiveInput VALUE", archiveInput.value);
