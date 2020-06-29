23a0d3fb9b9656b825c7bf426be3263b1ba5e262
Saqib Ameen <mrsaqibameen@gmail.com>
2020-06-30 02:19:12 +0500

    📦 NEW: bare minimum working version

--- githubExtends.js
+++ githubExtends.js
@@ -1,15 +1,33 @@
 // Get all the input fields on the settings page.
-let allInputs = document.querySelectorAll('p > input');
+const allInputs = document.querySelectorAll('p > input');
 
-let archiveInput = allInputs[3];
+// Trim extra input — all required ones has 2 classes.
+const filteredInputs = Array.from(allInputs).filter(e => e.classList.length === 2);
 
-console.log("archiveInput", archiveInput);
+// Grab the repo name.
+const inputValue = document.location.href.split('/').slice(3,5).join('/');
 
-// Select the input.
-archiveInput.value = 'saqibameen/learn-next-js';
-archiveInput.focus();
+// Attach the button.
+const updatedInputs = filteredInputs.map(e => {
+    const btn = createBtn();
+    e.parentElement.classList.add('sa-flex');
+    e.parentElement.appendChild(btn);
+})
 
-document.execCommand('insertText', false, 'saqibameen/learn-next-js');
+function createBtn() {
+    const btn = document.createElement('img');
+    btn.classList.add('sa-icon');
+    btn.src = 'https://clipboardjs.com/assets/images/clippy.svg';
+    btn.addEventListener('click', function(e) {
+        e.target.parentElement.firstElementChild.value = inputValue;
+        simulateChangeEvent(btn);
+    });
+    return btn;
+}
 
-// Check the value
-console.log("archiveInput VALUE", archiveInput.value);
+function simulateChangeEvent(el) {
+    el.dispatchEvent(
+        new Event('change', {
+            bubbles: true,
+    }))
+}
